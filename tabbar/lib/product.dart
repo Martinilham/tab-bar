12af2b15bc2774148ac120d36064dd2b6defbd32
Martin <ilhammartin2@gmail.com>
2024-03-13 21:00:20 +0700

    p

--- tabbar/lib/product.dart
+++ tabbar/lib/product.dart
@@ -37,10 +37,8 @@ class MyCard extends StatelessWidget{
                 left:4.0,
                 right:4.0
               )),
-            Icon(
-              icon,
-              size: 50.0,
-            ),
+            const Image(
+              image: NetworkImage("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.halodoc.com%2Fartikel%2Fini-7-manfaat-konsumsi-daging-buah-kelapa-untuk-buka-puasa&psig=AOvVaw1yf3A6ON0zZukHEP2gI-tS&ust=1710315246437000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCIDEmPma7oQDFQAAAAAdAAAAABAE")),
             Text(
               teks
             )
